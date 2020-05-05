e36b594e5e7b69d5f2d1fd66389223eb84ed4d0c
Victor <adevic4christ@gmail.com>
2020-05-05 21:22:20 +0100

    Created sng-v3 branch for payment using rave

--- lib/footer.php
+++ lib/footer.php
@@ -1,8 +1,8 @@
-  <p>
-  <a href="index.php">Home</a> |
-  <a href="login.php">Login</a>
-   | <a href="register.php">Register</a> | 
-   <a href="forgot.php">Forgot Password</a>
-  </p>
+
+<!-- MENU -->
+
+
 </body>
+
+
 </html>
